0b2116aa651406adcc9b7039d6b2034227901f27
hnimt <tongminh23996@gmail.com>
2021-07-05 20:53:49 +0700

    Ex4

---
+++ lab04.Stream/lab04.Stream/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+  public static void main(String[] args) {
+    /*DemoStream.map01();
+    DemoStream.map02();
+    DemoStream.map03();*/
+    //DemoStream.filter01();
+    //DemoStream.maxBy();
+    //DemoStream.groupBy01();
+    // DemoStream.distinct();
+    // DemoStream.map01();
+    // DemoStream.summingInt();
+    // DemoStream.getByName(10, 30);
+    DemoStream.getAverageAgeByNation();
+  }
+}
